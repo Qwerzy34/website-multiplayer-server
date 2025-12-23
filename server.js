7a8263c0c607a5918d7a0025194d7f8f8ad8cad5
Qwerzy34 <nikolas.adamski@outlook.com>
2025-12-23 16:44:33 +0000

    Server code bug fixes

--- server.js
+++ server.js
@@ -43,4 +43,4 @@ setInterval(() => {
 }, 50);
 
 const port = process.env.PORT || 8080;
-http.listen(port, () => console.log(`Server running on ${port}`))
+http.listen(port, '0.0.0.0', () => console.log(`Server running on ${port}`))
